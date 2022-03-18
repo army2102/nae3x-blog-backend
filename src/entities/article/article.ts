8cdd8102fceb0e677e58584360beac14fa5e850c
army2102 <naetirat.s@gmail.com>
2022-03-18 15:31:14 +0700

    Fix wrong variables & files' name

--- src/entities/article/article.ts
+++ src/entities/article/article.ts
@@ -2,17 +2,16 @@ import { v4 as uuidv4 } from 'uuid'
 
 import { Entity } from '@/entities/commons/entity.interface'
 
-class Arcticle implements Entity {
+class Article implements Entity {
   public readonly id: string
   private createdAt: Date
   private updatedAt: Date
 
   constructor(private content: string) {
     this.id = uuidv4()
-    this.content = content
     this.createdAt = new Date()
     this.updatedAt = new Date()
   }
 }
 
-export { Arcticle }
+export { Article }
