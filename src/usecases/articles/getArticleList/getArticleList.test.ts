d42d42b19bb5c5f79bd276df8d6ba9517b47b1bf
army2102 <naetirat.s@gmail.com>
2022-03-25 01:10:34 +0700

    Fix wrong test pattern

--- src/usecases/articles/getArticleList/getArticleList.test.ts
+++ src/usecases/articles/getArticleList/getArticleList.test.ts
@@ -8,68 +8,70 @@ describe('Test getArticleList', () => {
     articleRepository = jest.fn() as unknown as ArticleRepositoryInterface
   })
 
-  it('Should return output correctly', async () => {
-    const mockFindResult = {
-      articles: [
-        {
-          id: '1',
-          title: 'This is a title',
-          content: 'This is a content',
-          createdAt: '2022-03-18T08:45:21.963Z',
-          updatedAt: '2022-03-18T09:00:00.000Z'
-        },
-        {
-          id: '2',
-          title: 'This is a title',
-          content: 'This is a content',
-          createdAt: '2022-03-18T08:45:21.963Z',
-          updatedAt: '2022-03-18T09:00:00.000Z'
-        },
-        {
-          id: '3',
-          title: 'This is a title',
-          content: 'This is a content',
-          createdAt: '2022-03-18T08:45:21.963Z',
-          updatedAt: '2022-03-18T09:00:00.000Z'
-        },
-        {
-          id: '4',
-          title: 'This is a title',
-          content: 'This is a content',
-          createdAt: '2022-03-18T08:45:21.963Z',
-          updatedAt: '2022-03-18T09:00:00.000Z'
-        },
-        {
-          id: '5',
-          title: 'This is a title',
-          content: 'This is a content',
-          createdAt: '2022-03-18T08:45:21.963Z',
-          updatedAt: '2022-03-18T09:00:00.000Z'
-        }
-      ],
-      total: 10
-    }
-    articleRepository.findMany = jest.fn().mockResolvedValue(mockFindResult)
-    const useCase = new GetArticleList(articleRepository)
-
-    const expected = {
-      articles: mockFindResult.articles.map(
-        ({ id, title, content, createdAt }) => {
-          return {
-            id,
-            title,
-            content,
-            createdAt
+  describe('Test execute', () => {
+    it('Should return output correctly', async () => {
+      const mockFindResult = {
+        articles: [
+          {
+            id: '1',
+            title: 'This is a title',
+            content: 'This is a content',
+            createdAt: '2022-03-18T08:45:21.963Z',
+            updatedAt: '2022-03-18T09:00:00.000Z'
+          },
+          {
+            id: '2',
+            title: 'This is a title',
+            content: 'This is a content',
+            createdAt: '2022-03-18T08:45:21.963Z',
+            updatedAt: '2022-03-18T09:00:00.000Z'
+          },
+          {
+            id: '3',
+            title: 'This is a title',
+            content: 'This is a content',
+            createdAt: '2022-03-18T08:45:21.963Z',
+            updatedAt: '2022-03-18T09:00:00.000Z'
+          },
+          {
+            id: '4',
+            title: 'This is a title',
+            content: 'This is a content',
+            createdAt: '2022-03-18T08:45:21.963Z',
+            updatedAt: '2022-03-18T09:00:00.000Z'
+          },
+          {
+            id: '5',
+            title: 'This is a title',
+            content: 'This is a content',
+            createdAt: '2022-03-18T08:45:21.963Z',
+            updatedAt: '2022-03-18T09:00:00.000Z'
           }
-        }
-      ),
-      total: 10
-    }
-    const actual = await useCase.execute({
-      limit: 5,
-      offset: 0
-    })
+        ],
+        total: 10
+      }
+      articleRepository.findMany = jest.fn().mockResolvedValue(mockFindResult)
+      const useCase = new GetArticleList(articleRepository)
 
-    expect(actual).toEqual(expected)
+      const expected = {
+        articles: mockFindResult.articles.map(
+          ({ id, title, content, createdAt }) => {
+            return {
+              id,
+              title,
+              content,
+              createdAt
+            }
+          }
+        ),
+        total: 10
+      }
+      const actual = await useCase.execute({
+        limit: 5,
+        offset: 0
+      })
+
+      expect(actual).toEqual(expected)
+    })
   })
 })
