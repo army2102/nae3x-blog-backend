e4ea16be1a4b683ae00aba401c470bbd772fd566
army2102 <naetirat.s@gmail.com>
2022-03-25 11:20:30 +0700

    Add template for unimplement functions

--- src/repositories/articles/articleRepository.ts
+++ src/repositories/articles/articleRepository.ts
@@ -11,7 +11,26 @@ class ArticleRepository implements ArticleRepositoryInterface {
     throw new Error('Not Implement')
   }
 
+  public async findOne(id: string): Promise<Article> {
+    throw new Error('Not Implement')
+  }
+
   public async add(title: string, content: string): Promise<string> {
+    throw new Error('Not Implement')
+  }
+
+  public async update(
+    id: string,
+    updateData: {
+      title?: string | undefined
+      content?: string | undefined
+      thumbnail?: string | undefined
+    }
+  ): Promise<Article> {
+    throw new Error('Not Implement')
+  }
+
+  public async delete(id: string): Promise<void> {
     throw new Error('Not Implement')
   }
 }
