d849c12bcaf23e063f08ca687442f2a5d7f265c9
army2102 <naetirat.s@gmail.com>
2022-04-21 05:22:46 +0700

    Add unittest for create Article handler

---
+++ src/presenters/articles/createArticle/createArticleHandler.test.ts
@@ -0,0 +1,44 @@
+import 'reflect-metadata'
+import Container from 'typedi'
+
+import { CreateArticleInterface } from '@/usecases/articles/createArticle/createArticle.interface'
+import { CreateArticleHandler } from './createArticleHandler.impl'
+import { CreateArticle } from '@/usecases/articles/createArticle/createArticle.impl'
+
+describe('Test CreateArticleHandler', () => {
+  let createArticleUseCase: CreateArticleInterface
+
+  beforeEach(() => {
+    createArticleUseCase = jest.fn() as unknown as CreateArticleInterface
+    // TODO: Make @Inject work
+    Container.set(CreateArticle, createArticleUseCase)
+  })
+
+  describe('Test execute', () => {
+    it('Should create Article correctly', async () => {
+      const request = {
+        body: {
+          title: 'This is a title',
+          content: 'This is a content',
+          thumbnail: 'This is a thumbnail'
+        }
+      } as any
+      const mockSend = jest.fn()
+      const reply = {
+        status: jest.fn().mockReturnValue({ send: mockSend })
+      } as any
+      createArticleUseCase.execute = jest.fn().mockResolvedValue({ id: '1' })
+      const handler = new CreateArticleHandler(createArticleUseCase)
+
+      await handler.execute(request, reply)
+
+      expect(createArticleUseCase.execute).toBeCalledWith({
+        title: request.body.title,
+        content: request.body.content,
+        thumbnail: request.body.thumbnail
+      })
+      expect(reply.status).toBeCalledWith(201)
+      expect(mockSend).toBeCalledWith({ id: '1' })
+    })
+  })
+})
